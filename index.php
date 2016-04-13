1038f870e6eba57eca3f6b3c02716c212c11f9d9
Issam Benali <issam.ben-ali@kyalis.com>
2016-04-13 17:58:38 +0100

    Ajout de la page index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>SIGMA - Medical Administrators International</title>
+<link rel="stylesheet" href="maistyle.css" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<META NAME="AUTHOR" CONTENT="Sebastien Le Coz - MAI Core Team">
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+</head>
+
+<body>
+<table width="750" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="750" height="110" valign="top">je test</td>
+  </tr>
+</table>
+</body>
+</html>
